589b54525b54542c3426d317b848151fff698140
eiromz <eronmonsele.adeyemo@gmail.com>
2024-01-24 23:06:53 +0100

    added resource

--- src/Customer/App/Http/Resources/CustomerResource.php
+++ src/Customer/App/Http/Resources/CustomerResource.php
@@ -2,6 +2,7 @@
 
 namespace Src\Customer\App\Http\Resources;
 
+use App\Models\KnowYourCustomer;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -28,6 +29,30 @@ class CustomerResource extends JsonResource
             'is_member' => $this->is_member,
             'image' => $this->image,
             'profile' => (new ProfileResource($this->whenLoaded('profile'))),
+            'pin-set' => $this->pinSet(),
+            'kyc-complete' => $this->kycComplete(),
         ];
+    }
+
+    public function pinSet(): bool
+    {
+        $boolean = false;
+        if(!is_null($this->transaction_pin)){
+            $boolean = true;
+        }
+        return $boolean;
+    }
+    public function kycComplete(): bool
+    {
+        $kyc = KnowYourCustomer::query()
+            ->where('customer_id','=',$this->id)
+            ->where('status','=',1)
+            ->first();
+        $boolean = false;
+
+        if(!is_null($this->transaction_pin)){
+            $boolean = true;
+        }
+        return $boolean;
     }
 }
