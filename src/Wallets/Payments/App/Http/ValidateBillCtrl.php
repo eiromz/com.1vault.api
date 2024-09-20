3171d41a2cf8eb89b123832980888290bf950116
eiromz <eronmonsele.adeyemo@gmail.com>
2024-09-20 07:44:34 +0100

    added error logging

--- src/Wallets/Payments/App/Http/ValidateBillCtrl.php
+++ src/Wallets/Payments/App/Http/ValidateBillCtrl.php
@@ -7,34 +7,45 @@ use App\Http\Controllers\DomainBaseCtrl;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Saloon\Exceptions\Request\FatalRequestException;
 use Src\Wallets\Payments\Domain\Integrations\Providus\ProvidusBills;
 use Src\Wallets\Payments\Domain\Integrations\Providus\Requests\ValidateBill;
 use Symfony\Component\HttpFoundation\Response;
 
 class ValidateBillCtrl extends DomainBaseCtrl
 {
+    private $response;
     /**
      * @throws Exception
      */
     public function __invoke($bill, Request $request): JsonResponse
     {
-        $request->merge(['bill' => $bill]);
+        try {
+            $request->merge(['bill' => $bill]);
 
-        $request->validate([
-            'inputs' => 'required|array',
-        ]);
+            $request->validate([
+                'inputs' => 'required|array',
+                'bill' => 'required',
+            ]);
 
-        $connector = new ProvidusBills;
-        $response = $connector->send(new ValidateBill($request->all(), $bill));
+            $connector = new ProvidusBills;
+            $this->response = $connector->send(new ValidateBill($request->all(), $bill));
 
-        if ($response->failed()) {
-            logger()->error('ValidateBillCtrl',[$response->body()]);
-            throw new BaseException(
-                'Invalid Customer Details',
-                Response::HTTP_BAD_REQUEST
-            );
+            if ($this->response->failed()) {
+                logger()->error('ValidateBillCtrl',[$this->response->body()]);
+                throw new BaseException(
+                    'Invalid Customer Details',
+                    Response::HTTP_BAD_REQUEST
+                );
+            }
+
+            return jsonResponse(Response::HTTP_OK, $this->response->json());
+        }catch (Exception|FatalRequestException $e){
+            logger('ValidateBillCtrl',[$e->getMessage()]);
+
+            return jsonResponse(Response::HTTP_BAD_REQUEST,[
+                'message' => 'We could not process your request at this time'
+            ]);
         }
-
-        return jsonResponse(Response::HTTP_OK, $response->json());
     }
 }
